48a485bc796f35dddc51432ee25cd50042d29234
Plastix <aidanpieper@gmail.com>
2014-05-20 18:46:05 -0400

    Minor clean-up

--- js/game/main.js
+++ js/game/main.js
@@ -1,38 +1,35 @@
 require(["gameScreen"], function(gameScreen) {
 
-    //TODO: 
-    // Different types of enemies
-    // Menu/Death screen!
-    // Score text (floating)
     // requestAnimationFrame polyfill by Erik Möller. Fixes from Paul Irish and Tino Zijdel
     // MIT license
     (function() {
         var lastTime = 0;
         var vendors = ['ms', 'moz', 'webkit', 'o'];
-        for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-            window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-            window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
-                                       || window[vendors[x]+'CancelRequestAnimationFrame'];
+        for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+            window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+            window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
         }
-     
+
         if (!window.requestAnimationFrame)
             window.requestAnimationFrame = function(callback, element) {
                 var currTime = new Date().getTime();
                 var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-                var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
-                  timeToCall);
+                var id = window.setTimeout(function() {
+                        callback(currTime + timeToCall);
+                    },
+                    timeToCall);
                 lastTime = currTime + timeToCall;
                 return id;
             };
-     
+
         if (!window.cancelAnimationFrame)
             window.cancelAnimationFrame = function(id) {
                 clearTimeout(id);
             };
     }());
 
-    var canvas = document.getElementById("stage");;
-    var surface = canvas.getContext("2d");;
+    var canvas = document.getElementById("stage");
+    var surface = canvas.getContext("2d");
     var currentScreen = gameScreen;
 
     canvas.setAttribute("width", 800);
